2c21e44e06a0e715c552b3ec66e4faf3fddc6983
Ivan Diniz <ivandinizaj@gmail.com>
2018-10-24 16:15:19 -0300

    Separação por métodos do Header, Section e Footer

--- src/com/imobiliaria/tabajara/GUI.java
+++ src/com/imobiliaria/tabajara/GUI.java
@@ -11,7 +11,7 @@ public class GUI extends JFrame implements ActionListener {
                         text_area,
                         text_garages,
                         text_typeImmobile,
-                        text_room,
+                        text_countRoom,
                         text_price,
                         text_countBathroom;
 
@@ -20,85 +20,113 @@ public class GUI extends JFrame implements ActionListener {
                     btn_last,
                     btn_first;
 
-    private int sizeColumn;
+    private JPanel panelHeader,
+                    panelSection,
+                    panelFooter;
 
+    private static final int SIZECOLUMN = 12;
+
+    private static final int HPAG = 3;
+
+    private static final int VPAG = 15;
 
     public GUI(String title){
 
         super(title);
 
-        JPanel panel          = new JPanel( new BorderLayout() );
+        JPanel panel        = new JPanel( new BorderLayout() );
 
-        JPanel panelSection   = new JPanel( new FlowLayout(FlowLayout.LEFT) );
-        JPanel panelFooter    = new JPanel( new FlowLayout() );
+        this.panelHeader    = new JPanel( new GridLayout(2,1) );
+        this.panelSection   = new JPanel( new FlowLayout(FlowLayout.LEFT, HPAG, VPAG) );
+        this.panelFooter    = new JPanel( new FlowLayout() );
 
+        this.setPanelHeader();
+        this.setPanelSection();
+        this.setPanelFooter();
+
+        panel.add(panelHeader, BorderLayout.NORTH);
         panel.add(panelSection, BorderLayout.CENTER);
-
-        this.sizeColumn = 12;
-
-        JLabel textHeader = new JLabel("Imobiliária Tabajara \n");
-        textHeader.setFont( new Font("Arial", Font.PLAIN, 24) );
-
-        panel.add(textHeader, BorderLayout.NORTH);
-
-        JLabel lbl_code          = new JLabel("Código");
-        JLabel lbl_area          = new JLabel("Área");
-        JLabel lbl_garages       = new JLabel("Garagens");
-        JLabel typeImmobile      = new JLabel("Tipo do Móvel");
-        JLabel lbl_room          = new JLabel("Quartos");
-        JLabel lbl_price         = new JLabel("Preço do Imóvel");
-        JLabel lbl_countBathroom = new JLabel("Nº de Banheiros");
-
-        //Atribui configuraões aos componentes
-        this.setComponents();
-
-        panelSection.add(lbl_code);
-        panelSection.add(this.text_code);
-        panelSection.add(lbl_area);
-        panelSection.add(this.text_area);
-        panelSection.add(lbl_garages);
-        panelSection.add(this.text_garages);
-        panelSection.add(typeImmobile);
-        panelSection.add(this.text_typeImmobile);
-        panelSection.add(lbl_room);
-        panelSection.add(this.text_room);
-        panelSection.add(lbl_price);
-        panelSection.add(this.text_price);
-        panelSection.add(lbl_countBathroom);
-        panelSection.add(this.text_countBathroom);
-
-        panelFooter.add(this.btn_last);
-        panelFooter.add(this.btn_previous);
-        panelFooter.add(this.btn_next);
-        panelFooter.add(this.btn_first);
-
-        this.btn_last.addActionListener(this);
-        this.btn_previous.addActionListener(this);
-        this.btn_next.addActionListener(this);
-        this.btn_first.addActionListener(this);
-
         panel.add(panelFooter, BorderLayout.SOUTH);
 
         getContentPane().add(panel);
 
     }
 
-    public void  setComponents(){
+    private void setPanelHeader(){
+
+        //Configuração do título do cabeçalho
+        JLabel textHeader = new JLabel("Imobiliária Tabajara \n");
+        textHeader.setHorizontalAlignment(SwingConstants.CENTER);
+        textHeader.setFont( new Font("Arial", Font.BOLD, 36) );
+        textHeader.setForeground(Color.RED);
+        this.panelHeader.add(textHeader, BorderLayout.NORTH);
+
+        //Configuração do subtítulo do cabeçalho
+        JLabel subTextHeader = new JLabel("Seu imóvel está aqui");
+        subTextHeader.setHorizontalAlignment(SwingConstants.CENTER);
+        subTextHeader.setForeground(Color.RED);
+        this.panelHeader.add(subTextHeader, BorderLayout.NORTH);
+    }
+
+    private void setPanelSection(){
+
+        this.text_code          = new JTextField(SIZECOLUMN / 2);
+        this.text_area          = new JTextField( SIZECOLUMN );
+        this.text_garages       = new JTextField(SIZECOLUMN / 2);
+        this.text_typeImmobile  = new JTextField( SIZECOLUMN );
+        this.text_countRoom     = new JTextField(SIZECOLUMN / 2);
+        this.text_price         = new JTextField( SIZECOLUMN );
+        this.text_countBathroom = new JTextField(SIZECOLUMN / 2);
+
+        JLabel lbl_code          = new JLabel(" Código");
+        JLabel lbl_area          = new JLabel(" Área");
+        JLabel lbl_garages       = new JLabel(" Garagens");
+        JLabel typeImmobile      = new JLabel(" Tipo do Móvel");
+        JLabel lbl_countRoom     = new JLabel(" Nº de Quartos");
+        JLabel lbl_price         = new JLabel(" Preço do Imóvel");
+        JLabel lbl_countBathroom = new JLabel(" Nº de Banheiros");
+
+        panelSection.add(lbl_code);
+        panelSection.add(this.text_code);
+
+        panelSection.add(typeImmobile);
+        panelSection.add(this.text_typeImmobile);
+
+        panelSection.add(lbl_area);
+        panelSection.add(this.text_area);
+
+        panelSection.add(lbl_garages);
+        panelSection.add(this.text_garages);
+
+        panelSection.add(lbl_countRoom);
+        panelSection.add(this.text_countRoom);
+
+        panelSection.add(lbl_countBathroom);
+        panelSection.add(this.text_countBathroom);
+
+        panelSection.add(lbl_price);
+        panelSection.add(this.text_price);
+    }
+
+    private void setPanelFooter(){
 
         this.btn_first      = new JButton("Primeiro");
         this.btn_last       = new JButton("Último");
         this.btn_previous   = new JButton("Anterior");
         this.btn_next       = new JButton("Próximo");
 
-        this.text_code          = new JTextField(this.sizeColumn);
-        this.text_area          = new JTextField(this.sizeColumn);
-        this.text_garages       = new JTextField(this.sizeColumn);
-        this.text_typeImmobile  = new JTextField(this.sizeColumn);
-        this.text_room          = new JTextField(this.sizeColumn);
-        this.text_price         = new JTextField(this.sizeColumn);
-        this.text_countBathroom = new JTextField(this.sizeColumn);
+        this.panelFooter.add(this.btn_last);
+        this.panelFooter.add(this.btn_previous);
+        this.panelFooter.add(this.btn_next);
+        this.panelFooter.add(this.btn_first);
+
+        this.btn_last.addActionListener(this);
+        this.btn_previous.addActionListener(this);
+        this.btn_next.addActionListener(this);
+        this.btn_first.addActionListener(this);
 
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
